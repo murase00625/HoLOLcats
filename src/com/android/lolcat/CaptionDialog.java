2495737b4c9182f7d6773f5794b3f3879ccf585b
Morio Murase <melchior00@gmail.com>
2013-07-12 20:21:43 -0700

    Corrected package imports to use HoloEverywhere, not the support library.

--- src/com/android/lolcat/CaptionDialog.java
+++ src/com/android/lolcat/CaptionDialog.java
@@ -1,10 +1,11 @@
 package com.android.lolcat;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
+import org.holoeverywhere.app.AlertDialog;
+
+import org.holoeverywhere.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import org.holoeverywhere.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,6 +22,17 @@ public class CaptionDialog extends DialogFragment {
 	}
 	
 	public CaptionDialog() { }
+	
+		/* (non-Javadoc)
+	 * @see android.support.v4.app.DialogFragment#onCreate(android.os.Bundle)
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setStyle(DialogFragment.STYLE_NORMAL, android.R.style.Theme_Holo);
+	}
+	
+	
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
